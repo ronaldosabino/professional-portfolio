8c055f93598898964cae3dda90ace3fa157d9c34
Ronaldo Sabino <filiperonaldo1@gmail.com>
2026-08-04 10:53:20 -0300

    feat: implement app layout with navbar and footer components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,29 @@
 import { Component } from '@angular/core';
+import { Navbar } from './core/components/navbar/navbar';
+import { Hero } from './features/home/hero/hero';
+import { About } from './features/home/about/about';
+import { Skills } from './features/home/skills/skills';
+import { Experience } from './features/home/experience/experience';
+import { Education } from './features/home/education/education';
+import { Projects } from './features/home/projects/projects';
+import { Testimonials } from './features/home/testimonials/testimonials';
+import { Contact } from './features/home/contact/contact';
+import { Footer } from './core/components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [
+    Navbar,
+    Hero,
+    About,
+    Skills,
+    Experience,
+    Education,
+    Projects,
+    Testimonials,
+    Contact,
+    Footer,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
