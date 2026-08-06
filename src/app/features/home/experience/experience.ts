270463d2476ccd4de271f429bcaae2a7834e168b
Ronaldo Sabino <filiperonaldo1@gmail.com>
2026-08-06 12:39:06 -0300

    refactor: simplify app structure by removing unused components and updating app.html layout

--- src/app/features/home/experience/experience.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-experience',
-  imports: [],
-  templateUrl: './experience.html',
-  styleUrl: './experience.css',
-})
-export class Experience {}
