be1479877067d4afbcae92a34acdd8a083131fa7
Ronaldo Sabino <filiperonaldo1@gmail.com>
2026-08-06 13:26:35 -0300

    feat: update app structure to include Home component and enhance main layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Navbar } from './core/components/navbar/navbar';
 
 import { Footer } from './core/components/footer/footer';
+import { Home } from './features/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, Footer],
+  imports: [Navbar, Footer, Home],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
