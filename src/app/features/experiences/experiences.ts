f53e9451ba068eb7006c14c92c960e0da332bc8c
Ronaldo Sabino <filiperonaldo1@gmail.com>
2026-08-06 12:47:00 -0300

    feat: add experiences, home, and project-details components with associated styles and tests

---
+++ src/app/features/experiences/experiences.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experiences',
+  imports: [],
+  templateUrl: './experiences.html',
+  styleUrl: './experiences.css',
+})
+export class Experiences {}
