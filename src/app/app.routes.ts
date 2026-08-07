0936b064020e69a055d183c67cce3c26d607ce36
Ronaldo Sabino <filiperonaldo1@gmail.com>
2026-08-07 16:33:02 -0300

    feat: update routing and main template to use router outlet for dynamic component loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./features/home/home').then((c) => c.Home),
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./features/home/home').then((c) => c.Home),
+  },
+  {
+    path: 'experiences',
+    loadComponent: () => import('./features/experiences/experiences').then((c) => c.Experiences),
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { Navbar } from './core/components/navbar/navbar';
 import { Footer } from './core/components/footer/footer';
-import { Home } from './features/home/home';
+import { RouterOutlet } from '../../node_modules/@angular/router/types/_router_module-chunk';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, Footer, Home],
+  imports: [Navbar, Footer, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
