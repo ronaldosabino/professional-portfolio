3f2b3d4ccf65d94c8e981f5b3e9b0c78e506809f
Ronaldo Sabino <filiperonaldo1@gmail.com>
2026-08-07 16:42:42 -0300

    fix: correct import path for RouterOutlet in app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { Navbar } from './core/components/navbar/navbar';
 import { Footer } from './core/components/footer/footer';
-import { RouterOutlet } from '../../node_modules/@angular/router/types/_router_module-chunk';
-
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [Navbar, Footer, RouterOutlet],
