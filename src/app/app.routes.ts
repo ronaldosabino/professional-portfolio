3cac465987bb56562a21a72dd412bd2ce87098e0
Ronaldo Sabino <filiperonaldo1@gmail.com>
2026-08-10 17:59:30 -0300

    feat: add education section with timeline and styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,18 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./features/home/home').then((c) => c.Home),
+    loadComponent: () => import('./features/home/home').then((m) => m.Home),
   },
   {
     path: 'home',
-    loadComponent: () => import('./features/home/home').then((c) => c.Home),
+    loadComponent: () => import('./features/home/home').then((m) => m.Home),
   },
   {
     path: 'experiences',
-    loadComponent: () => import('./features/experiences/experiences').then((c) => c.Experiences),
+    loadComponent: () => import('./features/experiences/experiences').then((m) => m.Experiences),
+  },
+  {
+    path: 'education',
+    loadComponent: () => import('./features/education/education').then((m) => m.Education),
   },
 ];
