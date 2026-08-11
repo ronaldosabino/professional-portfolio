65989e7ec6eb17ac3126e1cabf1ad483b3449306
Ronaldo Sabino <filiperonaldo1@gmail.com>
2026-08-11 00:32:34 -0300

    feat: update about section with detailed content and resume download functionality

--- src/app/features/about/about.ts
+++ src/app/features/about/about.ts
@@ -6,4 +6,12 @@ import { Component } from '@angular/core';
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
-export class About {}
+export class About {
+  downloadResume() {
+    const resumeUrl = 'assets/Curriculo_Ronaldo_Sabino.pdf';
+    const link = document.createElement('a');
+    link.href = resumeUrl;
+    link.download = 'Curriculo_Ronaldo_Sabino.pdf';
+    link.click();
+  }
+}
