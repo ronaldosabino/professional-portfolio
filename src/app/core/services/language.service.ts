378a2f9538726eaae3a49570e9a8ee4e14f62029
Ronaldo Sabino <filiperonaldo1@gmail.com>
2026-08-28 17:06:00 -0300

    feat: enhance settings component with form controls for theme, language, and font size

--- src/app/core/services/language.service.ts
+++ src/app/core/services/language.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LanguageService {
-
-constructor() { }
-
+  constructor() {}
+  language: string | null = '';
+  theme: string | null = '';
+  fontSize: string | null = '';
 }
--- src/app/features/settings/settings.ts
+++ src/app/features/settings/settings.ts
@@ -1,9 +1,35 @@
 import { Component } from '@angular/core';
+import { LanguageService } from '../../core/services/language.service';
+import { FormBuilder, FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-settings',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './settings.html',
   styleUrl: './settings.css',
 })
-export class Settings {}
+export class Settings {
+  constructor(protected languageService: LanguageService) {}
+
+  form = new FormGroup({
+    theme: new FormControl('system'),
+    language: new FormControl('portuguese'),
+    fontSize: new FormControl('normal'),
+  });
+
+  changeTheme() {
+    this.languageService.theme = this.form.get('theme')!.value;
+    console.log(this.languageService.theme);
+  }
+
+  changeLanguage() {
+    this.languageService.language = this.form.get('language')!.value;
+    console.log(this.languageService.language);
+  }
+
+  changeFontSize() {
+    this.languageService.fontSize = this.form.get('fontSize')!.value;
+    console.log(this.languageService.fontSize);
+  }
+}
